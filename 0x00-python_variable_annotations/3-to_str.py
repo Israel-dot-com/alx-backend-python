b80f9523dfe5fc29d07846859405a8bc3b584627
Israel <isltimi1000@gmail.com>
2023-03-31 23:08:02 +0100

    Add task 3-5 and others i'll redo

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''Module containing solution to task 3.
+'''
+
+
+def to_str(n: float) -> str:
+    '''Converts a floating-point number to a string.
+    '''
+    return str(n)
---
+++ 0x00-python_variable_annotations/4-define_variable.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''Module containing solution to task 4.
+'''
+
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = 'Holberton'
