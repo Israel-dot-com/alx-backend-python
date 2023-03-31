3aab1e27d047c608fa779838904296d88366efd3
Israel <isltimi1000@gmail.com>
2023-03-31 22:50:15 +0100

    Validate pycodestyle

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-'''This module takes two arguments, adds them and returns it's value as a float.
+'''This module adds two float
 '''
+
 
 def add(a: float, b: float) -> float:
     '''This function adds two floating point numbers and returns a float.
