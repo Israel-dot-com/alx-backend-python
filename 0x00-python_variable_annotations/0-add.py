6ee0f724f2fe0a6cdabf2e9030bddbd46e09ef03
Israel <isltimi1000@gmail.com>
2023-03-31 16:56:23 +0100

    Pycode Validation

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-'''This module takes two arguments, adds them and returns it's value as a float
+'''This module takes two arguments, adds them and returns it's value as a float.
 '''
 
 def add(a: float, b: float) -> float:
-    '''This function adds two floating point numbers and returns a float
+    '''This function adds two floating point numbers and returns a float.
     '''
+
     return a + b
