44e9ff61dd7cd20687fb6c410a97e615b9e4b4c7
Israel <isltimi1000@gmail.com>
2023-03-31 16:53:06 +0100

    Update task 0

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 '''This module takes two arguments, adds them and returns it's value as a float
 '''
-def add(a: float, b: float) -> float:
-    print( a + b )
 
-add(5, 5)
+def add(a: float, b: float) -> float:
+    '''This function adds two floating point numbers and returns a float
+    '''
+    return a + b
