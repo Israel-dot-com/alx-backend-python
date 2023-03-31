b6d6ff253d660971a0cc4327478f9f97ae021677
Israel <isltimi1000@gmail.com>
2023-03-31 23:01:23 +0100

    add task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''This module calculates the floor of an fpn.
+'''
+
+
+def floor(a: float) -> int:
+    '''Function to calculate the floor of a floating-point number.
+    '''
+    return int(a)
